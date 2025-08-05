d80a7a6feeb35bdee0d7e8dbf7e876c6dd3d83f0
Ashish Shah <eiepixxstuff@gmail.com>
2025-08-05 16:34:58 +0530

    Vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,19 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import path from 'path'
 
-// ESM-compatible __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: import.meta.env.VITE_BASE_PATH || '/',
+  base: process.env.VITE_BASE_PATH || '/',
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 });
